fa809174510de0f0b4f5f5d80c9802ad5c31413f
quanxin <297451@qq.com>
2025-02-13 15:42:27 +0800

    solve conflict

--- code/data_process.py
+++ code/data_process.py
@@ -7,6 +7,11 @@ import pandas as pd
 data_path = '../assets/初赛数据/'
 
 def merge_csv_files(folder_path, out_path):
+    """
+    将两个时间跨度的表合并为一张表
+    :param folder_path: 原始数据路径.
+    :param out_path: 转换结果路径.
+    """
     # Store files by their prefix
     file_groups = defaultdict(list)
     for file_name in os.listdir(folder_path):
@@ -24,4 +29,36 @@ def merge_csv_files(folder_path, out_path):
         merged_df.to_csv(output_file, index=False)
         print(f'Merged files with prefix "{prefix}" into {output_file}')
 
+def convert_to_numeric(value):
+    """
+    将字符串转换为数字，如果无法转换则返回-1
+    """
+    try:
+        return float(value)
+    except ValueError:
+        return -1
+
+
 merge_csv_files(data_path, 'data')
+
+
+
+# 给A架打标签
+df = pd.read_csv('data/Ajia_plc_1.csv')
+# A架开机
+df['Ajia-3_v'] = df['Ajia-3_v'].apply(convert_to_numeric)
+df['Ajia-5_v'] = df['Ajia-5_v'].apply(convert_to_numeric)
+df['status'] = 'False'
+df['check_current_presence'] = 'False'
+for i in range(1, df.shape[0]):
+    if (df.loc[i - 1, 'Ajia-3_v'] == -1 and (df.loc[i, 'Ajia-3_v'] >= 0)) and (df.loc[i - 1, 'Ajia-5_v'] == -1 and (df.loc[i, 'Ajia-5_v'] >= 0)):
+        df.loc[i, 'status'] = '开机'
+    if (df.loc[i, 'Ajia-3_v'] == -1 and df.loc[i - 1, 'Ajia-3_v']== 0) or (df.loc[i, 'Ajia-5_v'] == -1 and df.loc[i - 1, 'Ajia-5_v']== 0):
+        df.loc[i, 'status'] = '关机'
+    
+    if (df.loc[i, 'Ajia-5_v'] > 0 and df.loc[i - 1, 'Ajia-5_v'] == -1):
+        df.loc[i, 'check_current_presence'] = '有电流'
+    if df.loc[i, 'Ajia-5_v'] > 0 and (df.loc[i - 1, 'Ajia-5_v'] == 0 or df.loc[i - 1, 'Ajia-5_v'] == '0'):
+        df.loc[i, 'check_current_presence'] = '有电流'
+    if df.loc[i, 'Ajia-5_v'] == 0 and (df.loc[i - 1, 'Ajia-5_v'] > 0 or df.loc[i - 1, 'Ajia-5_v'] == '0'):
+        df.loc[i, 'check_current_presence'] = '无电流'
