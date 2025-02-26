cf4242d921db0ef2f26bcf5caf33fb3e339a0279
quanxin <297451@qq.com>
2025-02-26 21:41:55 +0800

    update

--- utils.py
+++ utils.py
@@ -65,6 +65,7 @@ def get_completion(
     tools: list[dict] = [],
     model: str = "glm-4-plus",
     temperature: float = 0,
+    json_output: bool = False,
 ) -> Completion | StreamResponse[ChatCompletionChunk]:
     """
     获得对话结果
@@ -77,11 +78,17 @@ def get_completion(
     try:
         client = ZhipuAI(api_key=check_api_key())
         logger.trace("【请求回答】", str(messages), "【工具】", str(tools))
+        if json_output:
+            response_format = {'type': 'json_object'}
+        else:
+            response_format = {'type': 'text'}
         response = client.chat.completions.create(
             model=model,
             stream=False,
             messages=messages,
             tools=tools,
+            response_format=response_format,
+            temperature=temperature,
         )
         logger.trace("【回答结果】", str(response))
         return response
