278b62d2942198d56b9c2602002ee7e3051b1670
Robert <yutuan.victory@gmail.com>
2025-03-11 22:02:22 +0800

    Update docker

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM hubdocker.aminer.cn/library/python-base:1.0.0
 
 COPY devlop_home /app/devlop_home/
 
-RUN pip install --no-cache-dir -r /app/devlop_home/requirements.txt
+RUN pip install --no-cache-dir -r /app/devlop_home/requirements.txt && \
+    rm -rf /root/.cache
 
 ARG ZHIPUAI_API_KEY
 ENV ZHIPUAI_API_KEY=${ZHIPUAI_API_KEY}
--- build.bat
+++ build.bat
@@ -1,17 +1,33 @@
 @echo off
 chcp 65001 >nul
-echo 检查 ZHIPUAI_API_KEY 是否已设置
+
 IF "%ZHIPUAI_API_KEY%"=="" (
     echo 错误：未找到 ZHIPUAI_API_KEY 环境变量
     pause
     exit /b 1
 )
 
-@REM echo 生成数据处理脚本
-@REM jupyter nbconvert --to script devlop_home/data_process.ipynb
+IF "%1"=="" (
+    echo 错误：未指定用户
+    pause
+    exit /b 1
+)
+
+set ACCOUNT=%1
+
+IF "%2"=="" (
+    echo 错误：未指定镜像标签
+    pause
+    exit /b 1
+)
+
+set TAG=%2
+
+echo 生成数据处理脚本
+jupyter nbconvert --to script devlop_home/data_process.ipynb
 
 echo 构建 Docker 镜像
-docker build --build-arg ZHIPUAI_API_KEY=%ZHIPUAI_API_KEY% -t hubdocker.aminer.cn/013861b58d084a79866ded8df8801da1/qingjing:0.0.1 .
+docker build --build-arg ZHIPUAI_API_KEY=%ZHIPUAI_API_KEY% -t hubdocker.aminer.cn/%ACCOUNT%/%TAG% .
 
 echo Docker 镜像构建完成
-pause
+docker push hubdocker.aminer.cn/%ACCOUNT%/%TAG%
