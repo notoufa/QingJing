cd702051c020327a9356e496292fb53725c05f26
Robert <yutuan.victory@gmail.com>
2025-03-11 21:45:52 +0800

    Update dockerfile

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM hubdocker.aminer.cn/library/python-base:1.0.0
+
+COPY devlop_home /app/devlop_home/
+
+RUN pip install --no-cache-dir -r /app/devlop_home/requirements.txt
+
+ARG ZHIPUAI_API_KEY
+ENV ZHIPUAI_API_KEY=${ZHIPUAI_API_KEY}
+
+WORKDIR /app/
--- build.bat
+++ build.bat
@@ -1,14 +1,17 @@
 @echo off
-REM 检查 ZHIPUAI_API_KEY 是否已设置
+chcp 65001 >nul
+echo 检查 ZHIPUAI_API_KEY 是否已设置
 IF "%ZHIPUAI_API_KEY%"=="" (
     echo 错误：未找到 ZHIPUAI_API_KEY 环境变量
     pause
     exit /b 1
 )
 
-REM 构建 Docker 镜像
-docker build --build-arg ZHIPUAI_API_KEY=%ZHIPUAI_API_KEY% -t my-image .
+@REM echo 生成数据处理脚本
+@REM jupyter nbconvert --to script devlop_home/data_process.ipynb
 
-REM 提示完成
+echo 构建 Docker 镜像
+docker build --build-arg ZHIPUAI_API_KEY=%ZHIPUAI_API_KEY% -t hubdocker.aminer.cn/013861b58d084a79866ded8df8801da1/qingjing:0.0.1 .
+
 echo Docker 镜像构建完成
 pause
