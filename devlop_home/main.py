268ebb142f18bf08a5a3acb5ddc1bb55451cf610
quanxin <297451@qq.com>
2025-03-13 10:32:28 +0800

    update LF

--- devlop_home/main.py
+++ devlop_home/main.py
@@ -41,12 +41,14 @@ def process_one(line: dict, answer_list: list[dict] = None) -> VoteResult | dict
     # if id != "gysxdmx_00001":
     #     return {"id": id, "question": question, "answer": question}
     # answer = None
-    # if answer_list:
-    #     for item in answer_list:
-    #         if item["id"] == id:
-    #             answer = item["answer"]
-    #             break
-    # return {"id": id, "question": question, "answer": answer}
+    if answer_list:
+        for item in answer_list:
+            if item["id"] == id:
+                answer = item["answer"]
+                if isinstance(answer, dict):
+                    answer = json.dumps(answer, ensure_ascii=False)
+                break
+    return {"id": id, "question": question, "answer": answer}
     try:
         logger.info(f"【开始获取问题{id}的答案】", question)
         vote_res = api.vote(id, question, utils.module_config.vote_times).clone()
