78997116c8ac4ef456fdd191bfd16c128b4cf2d3
Robert <yutuan.victory@gmail.com>
2025-03-18 09:37:23 +0800

    Update

--- py_devlop.sh
+++ py_devlop.sh
@@ -8,10 +8,11 @@ echo "数据解压完成"
 cd /app/
 echo "开始执行数据处理"
 python3 /app/devlop_home/data_process.py
+# sleep 15
+
 echo "数据处理完成"
 
 echo "开始执行main.py"
 python3 /app/devlop_home/main.py $1 $2
 echo "main.py执行完成，输出结果如下："
 cat $2
-# sleep 50
