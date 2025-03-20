6a7d4ae9321b1d9c443f780989be0621ca7adf07
quanxin <297451@qq.com>
2025-03-20 21:08:01 +0800

    第二次提交

--- devlop_other/tools/merge_answer.py
+++ devlop_other/tools/merge_answer.py
@@ -6,6 +6,7 @@ from pathlib import Path
 input_files = [
     "submits/复赛A榜成绩/result_2025-03-19-第二次-52.4.jsonl",
     "submits/复赛A榜成绩/result_2025-03-20-第一次-55.09.jsonl",
+    "submits/复赛A榜成绩/试试又不会怎样_result_2025-03-20.jsonl"
 ]
 output_file = "devlop_output/compare.csv"
 
