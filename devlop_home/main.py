fd4ee79ea0f1cff73fc76072c957558755cecdce
quanxin <297451@qq.com>
2025-03-22 22:56:13 +0800

    update

--- devlop_home/main.py
+++ devlop_home/main.py
@@ -35,16 +35,16 @@ def process_one(line: dict) -> VoteResult | dict:
     int_id = int(id.split("_")[-1])
     question = handle_question(line["question"])
 
-    # with open(answer_filepath, "r", encoding="utf-8") as f:
-    #     answer_list = [json.loads(line.strip()) for line in f]
-    # answer = None
-    # for item in answer_list:
-    #     if item["id"] == id:
-    #         answer = item["answer"]
-    #         return {"id": id, "question": question, "answer": answer}
+    with open(answer_filepath, "r", encoding="utf-8") as f:
+        answer_list = [json.loads(line.strip()) for line in f]
+    answer = None
+    for item in answer_list:
+        if item["id"] == id:
+            answer = item["answer"]
+            return {"id": id, "question": question, "answer": answer}
     
-    # if int_id not in range(51, 76):
-    #     return {"id": id, "question": question, "answer": ""}
+    if int_id not in range(51, 76):
+        return {"id": id, "question": question, "answer": ""}
 
     try:
         logger.info(f"【开始获取问题{id}的答案】", question)
--- devlop_other/tools/merge_answer.py
+++ devlop_other/tools/merge_answer.py
@@ -5,9 +5,8 @@ from pathlib import Path
 
 input_files = [
     # "submits/复赛A榜成绩/result_2025-03-19-第二次-52.4.jsonl",
-    "submits/复赛A榜成绩/result_2025-03-20-第3次-55.27.jsonl",
-    "submits/复赛A榜成绩/confidence.jsonl",
-    "submits/复赛A榜成绩/result_2025-03-21-第3次.jsonl"
+    "submits/复赛A榜成绩/result_2025-03-21-55.9.jsonl",
+    "devlop_output/results/result_2025-03-22-第一次.jsonl"
 ]
 output_file = "devlop_output/compare.csv"
 
