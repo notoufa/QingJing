1acf6ab13c38ba1b09d8149d93f3ec8bec38268c
Robert <yutuan.victory@gmail.com>
2025-03-25 13:44:06 +0800

    Remove knowledge

--- devlop_other/tools/merge_answer.py
+++ devlop_other/tools/merge_answer.py
@@ -6,8 +6,8 @@ from pathlib import Path
 input_files = [
     # "submits/复赛A榜成绩/result_2025-03-19-第二次-52.4.jsonl",
     # "submits/复赛A榜成绩/result_2025-03-21-55.9.jsonl",
-    "submits/复赛A榜成绩/result_2025-03-22-62.38.jsonl",
-    "submits/复赛A榜成绩/result_2025-03-24-65.28.jsonl"
+    "submits/复赛A榜成绩/result_2025-03-24-65.28.jsonl",
+    "submits/复赛A榜成绩/result_2025-03-24-test.jsonl"
 ]
 output_file = "devlop_output/compare.csv"
 
